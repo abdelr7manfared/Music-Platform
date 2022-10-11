f9cca8726c635651afa21c9f2a8b0506c0db4ad9
abdulrahman <abdelr7manfared@gmail.com>
2022-10-11 09:30:19 +0200

    feat: Make Artits view & form

---
+++ artists/form.py
@@ -0,0 +1,7 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from .models import Artist
+class ArtistForm(ModelForm):
+    class Meta:
+        model = Artist
+        fields = '__all__'
